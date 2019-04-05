6543ce7e4f26e964fd2108356034212c48bc5fc7
root <root@localhost.localdomain>
2019-04-05 19:51:06 +0900

    bmp 테스트 성공

--- src/main/java/com/recognizer/Recognizer.java
+++ src/main/java/com/recognizer/Recognizer.java
@@ -1,7 +1,19 @@
 package com.recognizer;
 
+import java.io.File;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 public class Recognizer {
     public static void main(String[] args) {
-        System.out.println("entry test");
+        File img = new File("1.bmp");
+        Tesseract ts = new Tesseract();
+        ts.setDatapath("src/main/resources/tessdata");
+        try {
+            System.out.println(ts.doOCR(img));
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
     }
 }
