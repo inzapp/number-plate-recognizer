ddba64190e8fd7c4d265e466b72c1eaeb9130b89
inzapp <inzapp@naver.com>
2019-04-26 19:24:37 +0900

    FX Controller 추가

--- src/main/java/com/recognition/software/jdeskew/Recognizer.java
+++ src/main/java/com/recognition/software/jdeskew/Recognizer.java
@@ -1,6 +1,8 @@
 package com.recognition.software.jdeskew;
 
+import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import org.opencv.core.Core;
@@ -15,26 +17,83 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-abstract class View {
-	
-	public static ImageView imgView;
-	public static ListView<String> imgList;
-	public static Button addBt, removeBt, startBt;
-	public static Label resultLb;
+class View {
+
+	private ImageView imgView;
+	private ListView<String> imgList;
+	private Button addBt, removeBt, startBt;
+	private Label resultLb;
+
+	static class Singleton {
+		static View INSTANCE = new View();
+	}
+
+	public static View getInstance() {
+		return Singleton.INSTANCE;
+	}
+
+	public ImageView getImgView() {
+		return imgView;
+	}
+
+	public void setImgView(ImageView imgView) {
+		this.imgView = imgView;
+	}
+
+	public ListView<String> getImgList() {
+		return imgList;
+	}
+
+	public void setImgList(ListView<String> imgList) {
+		this.imgList = imgList;
+	}
+
+	public Button getAddBt() {
+		return addBt;
+	}
+
+	public void setAddBt(Button addBt) {
+		this.addBt = addBt;
+	}
+
+	public Button getRemoveBt() {
+		return removeBt;
+	}
+
+	public void setRemoveBt(Button removeBt) {
+		this.removeBt = removeBt;
+	}
+
+	public Button getStartBt() {
+		return startBt;
+	}
+
+	public void setStartBt(Button startBt) {
+		this.startBt = startBt;
+	}
+
+	public Label getResultLb() {
+		return resultLb;
+	}
+
+	public void setResultLb(Label resultLb) {
+		this.resultLb = resultLb;
+	}
 }
 
 interface EventInjector {
-	
+
 	public void injectView();
-	
+
 	public void injectEvent();
-	
-	public void clickAddBt();
-	
+
+	public void clickAddBt(Stage stage);
+
 	public void clickRemoveBt();
-	
+
 	public void clickStartBt();
 }
 
@@ -43,53 +102,62 @@ public class Recognizer extends Application implements Initializable, EventInjec
 	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
-	
+
+	private Stage stage;
+	private View view;
+
 	@FXML
 	private ImageView imgView;
-	
+
 	@FXML
 	private ListView<String> imgList;
-	
+
 	@FXML
 	private Button addBt, removeBt, startBt;
-	
+
 	@FXML
 	private Label resultLb;
-	
+
 	@Override
 	public void injectView() {
-		View.imgView = this.imgView;
-		View.imgList = this.imgList;
-		View.addBt = this.addBt;
-		View.removeBt = this.removeBt;
-		View.startBt = this.startBt;
-		View.resultLb = this.resultLb;
+		view.setImgView(imgView);
+		view.setImgList(imgList);
+		view.setAddBt(addBt);
+		view.setRemoveBt(removeBt);
+		view.setStartBt(startBt);
+		view.setResultLb(resultLb);
 	}
 
 	@Override
 	public void injectEvent() {
-		View.addBt.setOnAction(event -> this.clickAddBt());
-		View.removeBt.setOnAction(event -> this.clickRemoveBt());
-		View.startBt.setOnAction(event -> this.clickStartBt());
+		view.getAddBt().setOnMouseClicked(event -> this.clickAddBt(stage));
+		view.getRemoveBt().setOnMouseClicked(event -> this.clickRemoveBt());
+		view.getStartBt().setOnMouseClicked(event -> this.clickStartBt());
 	}
 
 	@Override
-	public void clickAddBt() {
-		
+	public void clickAddBt(Stage stage) {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("이미지를 선택하세요");
+		List<File> fileList = fileChooser.showOpenMultipleDialog(stage);
+		for(File curFile : fileList) {
+			System.out.println(curFile.getAbsolutePath());
+		}
 	}
 
 	@Override
 	public void clickRemoveBt() {
-		
+
 	}
 
 	@Override
 	public void clickStartBt() {
-		
+
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		view = View.getInstance();
 		injectView();
 		injectEvent();
 	}
@@ -101,20 +169,23 @@ public class Recognizer extends Application implements Initializable, EventInjec
 		primaryStage.setTitle("Number plate recognizer by inzapp");
 		primaryStage.setResizable(false);
 		primaryStage.show();
+
+		this.stage = primaryStage;
+		this.view = View.getInstance();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
-		
-		// File img = new File("1.bmp");
-        // Tesseract ts = new Tesseract();
-        // ts.setDatapath("src/main/resources/tessdata");
-        // try {
-        //     System.out.println(ts.doOCR(img));
-        // } catch (TesseractException e) {
-        //     e.printStackTrace();
-        // }
 
-        // Mat mat = new Mat();
+		// File img = new File("1.bmp");
+		// Tesseract ts = new Tesseract();
+		// ts.setDatapath("src/main/resources/tessdata");
+		// try {
+		// System.out.println(ts.doOCR(img));
+		// } catch (TesseractException e) {
+		// e.printStackTrace();
+		// }
+
+		// Mat mat = new Mat();
 	}
 }
